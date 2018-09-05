7a4c3cb494add8347347e936c9f6a9aa366728fc
duanxiaoqiu <Dxq#5201314>
2018-09-05 14:35:02 +0800

    init

---
+++ src/main/java/com/xq/controller/MainController.java
@@ -0,0 +1,23 @@
+package com.xq.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Package: com.xq.controller
+ * User: 段小秋
+ * Email: duanxiaoqiu@jd.com
+ * Date: 2018/9/5
+ * Time: 14:31
+ * Description:
+ */
+@Controller
+@RequestMapping("/main/")
+public class MainController {
+
+    @RequestMapping("/index.html")
+    public String index() {
+        return "index";
+    }
+
+}
