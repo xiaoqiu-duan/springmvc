ce54ef21424210cf00e1a9e2dc621429c3d169cc
duanxiaoqiu <Dxq#5201314>
2018-09-05 16:44:44 +0800

    修改url

--- src/main/java/com/xq/controller/MainController.java
+++ src/main/java/com/xq/controller/MainController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
  * Description:
  */
 @Controller
-@RequestMapping("/main/")
+@RequestMapping("/main")
 public class MainController {
 
     @RequestMapping("/index.html")
