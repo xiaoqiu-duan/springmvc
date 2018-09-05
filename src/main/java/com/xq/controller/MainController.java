9c6ad17d50fbe9c4831947677fb94b30a7f80658
duanxiaoqiu <Dxq#5201314>
2018-09-05 17:15:24 +0800

    edit

--- src/main/java/com/xq/controller/MainController.java
+++ src/main/java/com/xq/controller/MainController.java
@@ -2,6 +2,7 @@ package com.xq.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  * Package: com.xq.controller
@@ -15,6 +16,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/main")
 public class MainController {
 
+    @ResponseBody
     @RequestMapping("/index.html")
     public String index() {
         return "index";
